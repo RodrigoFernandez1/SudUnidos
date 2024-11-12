0bd06c460bf0e121ebb3488a760bf4925b91a597
rodrigoo.fernandezz <rodrigoo1.fernandezz@gmail.com>
2024-11-12 01:16:26 -0300

    mejoras y añadiendo img a los carts

--- Componentes js/strings.js
+++ Componentes js/strings.js
@@ -1,3 +1,9 @@
 document.querySelector(".card-button").addEventListener("click", () => {
   alert("¡Botón de la tarjeta presionado!");
 });
+
+/* nav */
+function toggleMenu() {
+  const navMenu = document.getElementById("navMenu");
+  navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
+}
